b34d723f271763c6652facb83481482b9d1d6dd0
devAyumi <ayumifabiane@gmail.com>
2025-08-28 12:26:43 -0300

    Alterado menu inicial e criado frame e painel de tela inteira. Falta adicionar estilização do background e colocar as perguntas dentro do painel, e não no JOptionPane

--- src/MenuInicial.java
+++ src/MenuInicial.java
@@ -1,24 +1,35 @@
 import javax.swing.*;
+import java.awt.*;
 
-// Classe principal que contém o metodo main
+// Classe principal que contém o método main
 public class MenuInicial {
     public static void main(String[] args){
-        String[] opcoes = {"INICIAR", "SAIR"};
-        int opcaoMenu = JOptionPane.showOptionDialog(
-                null,
-                null,
-                "QUIZ DO MEIO AMBIENTE",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                opcoes,
-                opcoes[0]
-        );
+        JFrame frame = new JFrame("Aplicativo em tela cheia"); // criação do frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setUndecorated(true); // remove bordas e barra de título
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // maximiza o frame horizontal e verticalmente (tela cheia)
 
-        if (opcaoMenu == 0){
-            Quiz.iniciarQuiz();
-        } else {
-            System.exit(0);
-        }
+        JPanel panel = new JPanel(); // criação do painel principal
+        panel.setBackground(Color.GRAY);
+        panel.setLayout(new GridBagLayout()); // define o gerenciador de layout como um GridBagLayout
+        // GridBagLayout organiza os componentes em grade de linhas e colunas
+
+        GridBagConstraints configBotoes = new GridBagConstraints();
+
+        configBotoes.gridy = 0;
+        configBotoes.anchor = GridBagConstraints.CENTER; // posição dentro da célula
+        configBotoes.insets = new Insets(10,10,10,10); // margens
+
+        JButton botaoIniciar = new JButton("INICIAR");
+        Utils.configuracaoBotao(botaoIniciar, () -> Quiz.iniciarQuiz()); // Chama o método Utils e os parâmetros botaoIniciar e o runnable de Utils
+        panel.add(botaoIniciar, configBotoes); // adiciona o botão "INICIAR" ao painel
+
+        configBotoes.gridy = 1;
+        JButton botaoSair = new JButton("SAIR");
+        Utils.configuracaoBotao(botaoSair, () -> System.exit(0));
+        panel.add(botaoSair, configBotoes); // adiciona o botão "SAIR" ao painel
+
+        frame.add(panel); // adiciona o painel ao frame
+        frame.setVisible(true); // torna o frame visível
     }
 }
